5dda1f9552a436311665ff16f0fd9fb4b9ac3b62
dajerr <157253672+dajerr@users.noreply.github.com>
2024-04-09 21:57:15 -0500

    initial commit

---
+++ nav.js
@@ -0,0 +1,7 @@
+const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+        const mobileMenu = document.getElementById('mobileMenu');
+
+        mobileMenuBtn.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+
